425b946f3ccb1c86bd761ae3ebe58279578096da
Bediko <bediko@chaosdorf.de>
2014-10-29 12:33:58 +0100

    Add skeleton source file

---
+++ hpl.c
@@ -0,0 +1,10 @@
+#include "second.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <mpe.h>
+
+int main(int argc, char** argv){
+	int matrix_size=3;
+	double L[matrix_size][matrix_size];
+}
